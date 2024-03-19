deb23b95be12ff630f95e47a32bbdce349f088b0
git12nazneen <lipinazneen65@gmail.com>
2024-03-19 17:41:19 +0600

    piechart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import Banner from './assets/component/Banner/Banner'
-
+import Footer from './assets/component/Footer/Footer'
 import Navbar from './assets/component/Navbar/Navbar'
+import Piecahrt from './assets/component/Piechart/Piecahrt'
 import Price from './assets/component/Price/Price'
 import Linechart from './assets/component/lineChart/Linechart'
 
@@ -18,9 +19,10 @@ function App() {
    <Linechart></Linechart>
    </div>
    <div className='flex-1'>
-    
+    <Piecahrt></Piecahrt>
    </div>
    </div>
+   <Footer></Footer>
     </>
   )
 }
